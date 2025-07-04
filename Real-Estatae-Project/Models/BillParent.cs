5735b387e03f669b43d6ba9eac0a9c35df9394fe
hend-elsayed-github <ehend0713@gmail.com>
2025-07-05 01:36:23 +0300

    classes modification

--- Real-Estatae-Project/Models/BillParent.cs
+++ Real-Estatae-Project/Models/BillParent.cs
@@ -1,4 +1,5 @@
 ﻿using Real_Estate_Project.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Real_Estatae_Project.Models
 {
@@ -10,5 +11,18 @@ namespace Real_Estatae_Project.Models
 
         // billParent- payment 1-1
         public virtual Payment payment { get; set; }
+
+
+        // billParent-bill 1-1
+
+        public virtual Bill bill { get; set; }
+
+        // billParent-rent 1-1
+
+        public virtual Rent rent { get; set; }
+
+        // billParent-maintenance 1-1
+
+        public virtual Maintenance maintenance { get; set; }
     }
 }

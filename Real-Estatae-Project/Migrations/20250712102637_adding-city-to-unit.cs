a6b1b0a809fd8eb3f0f6871fbe84ebdb41575871
hend-elsayed-github <ehend0713@gmail.com>
2025-07-12 13:36:24 +0300

    unit-account updates

---
+++ Real-Estatae-Project/Migrations/20250712102637_adding-city-to-unit.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Real_Estatae_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingcitytounit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "city",
+                table: "Units",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "city",
+                table: "Units");
+        }
+    }
+}

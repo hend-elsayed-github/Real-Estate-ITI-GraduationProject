a0f0f35a116f2db3be2a6fc62d2700f172e1cd9d
hend-elsayed-github <ehend0713@gmail.com>
2025-07-18 02:10:26 +0300

    start use for renter

---
+++ Real-Estatae-Project/Controllers/RenterController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Real_Estatae_Project.DTO;
+using Real_Estatae_Project.Repositories;
+using Real_Estate_Project.Models;
+using System.Security.Claims;
+
+namespace Real_Estatae_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RenterController : ControllerBase
+
+    {
+            IUserRepository _userRepository;
+        public RenterController(IUserRepository userRepository)
+            {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> startUse([FromBody] RenterSSNDTO renterSSN)
+        {
+            string renterId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(renterId) || !User.IsInRole("Renter"))
+            {
+                return Unauthorized();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Unit renterUnit = await _userRepository.getUnitBySSN(renterSSN);
+            if (renterUnit == null)
+            {
+                return NotFound(new { message = "Unit not found for the provided SSN." });
+            }
+            await _userRepository.setRenterCommunity(renterId, renterUnit);
+            await _userRepository.setRenterUnit(renterId, renterUnit.id);
+
+            return Ok(new
+            {
+                message = "Renter is now using the unit.",
+                unitId = renterUnit.id,
+                communityId = renterUnit.communityId
+            });
+        }
+    }
+}

92d14e43b3c52be32179fdbfea037b6d4a452450
mennaebrahimm <mennaebrahim601@gmail.com>
2025-07-20 18:14:49 +0300

    add payment

---
+++ Real-Estatae-Project/DTO/payment/PayMentDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Real_Estatae_Project.DTO.payment
+{
+    public class PayMentDTO
+    {
+    }
+}
---
+++ Real-Estatae-Project/Repositories/PaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Real_Estate_Project.Models;
+
+namespace Real_Estatae_Project.Repositories
+{
+    public class PaymentRepository:IPaymentRepository
+    {
+        private readonly ProjectContext _context;
+        public PaymentRepository(ProjectContext _Context)
+        {
+            _context = _Context;
+        }
+
+
+    }
+}

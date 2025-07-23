60e0c94d536b10261b21ca5d6f3bcad95e909167
Hend Elsayed <ehend0713@gmail.com>
2025-07-24 01:27:00 +0300

    Update IUserRepository.cs.cs

--- Real-Estatae-Project/Repositories/IUserRepository.cs.cs
+++ Real-Estatae-Project/Repositories/IUserRepository.cs.cs
@@ -17,10 +17,6 @@ namespace Real_Estatae_Project.Repositories
         Task<UserCommunityDTO> GetUserCommunity(string userId);
         Task<List<UserCommunityDTO>> GetTopActiveUsersByCommunityAsync(string userId);
 
-
-        Task setRenterCommunity(string renterId, Unit renterUnit);
-        Task setRenterUnit(string renterId, int renterUnitId);
-
         Task<ApplicationUser> FindByIdAsync(string userId);
         Task Update(ApplicationUser user);
     }
